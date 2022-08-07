bdc41573a91b067c070952addba9d2fa39e8d05e
Darek Skwierczyński <dariusz_skwierczynski.pl>
2022-08-07 22:47:34 +0200

    Add login & registration

---
+++ lib/account/auth_gate.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:wydatki/home/pages/home_page.dart';
+
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      initialData: FirebaseAuth.instance.currentUser,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const SignInScreen(
+            providerConfigs: [
+              EmailProviderConfiguration(),
+            ],
+          );
+        }
+
+        return const HomePage();
+      },
+    );
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:wydatki/account/auth_gate.dart';
 import 'package:wydatki/firebase_options.dart';
 
 void main() async {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const AuthGate(),
     );
   }
 }
