1edaac380425c9323f55ca30bb5620998512f6b7
Darek Skwierczyński <dariusz_skwierczynski.pl>
2022-08-13 22:04:11 +0200

    Remowe unused imports

--- lib/app/cubit/auth_gate_cubit.dart
+++ lib/app/cubit/auth_gate_cubit.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterfire_ui/auth.dart';
 
 part 'auth_gate_state.dart';
 
